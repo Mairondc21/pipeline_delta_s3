cf8e40d9b67cf20eb175bc9ceb2bcbe02160f499
Mairon Costa <duartemairon@gmail.com>
2026-03-19 02:01:54 -0300

    fix: ajustes na identacao do dbt

--- airflow/include/dbt_gold/models/gold_transacoes_semana.sql
+++ airflow/include/dbt_gold/models/gold_transacoes_semana.sql
@@ -13,19 +13,19 @@ cte_cliente_total_por_mes AS (
         SUM(bs.valor) AS total_transacao,
         CASE
             WHEN
-                flag_fim_semana = true::text
+                dt.flag_fim_semana = true::text
                 THEN 'Mais transações aos finais de semanas'
             ELSE 'Mais transações em dias de semana'
         END AS flag_fim_semana_2
     FROM
         cte_base AS bs
     INNER JOIN {{ source('s3_silver', 'dim_cliente') }} AS cl
-        ON bs.cliente_id = cl.cliente_id AND flag_atual = true
+        ON bs.cliente_id = cl.cliente_id AND cl.flag_atual = true
     LEFT JOIN {{ source('s3_silver','dim_data') }} AS dt
         ON bs.sk_date = dt.sk_date
     GROUP BY
         cl.nome,
-        flag_fim_semana
+        dt.flag_fim_semana
 )
 
 SELECT * FROM cte_cliente_total_por_mes
